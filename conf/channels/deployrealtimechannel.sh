f62e5720f793c5acb5c3d7e68280e61e4db87156
mdpettit <marshall.pettit@healthcatalyst.com>
2017-09-06 22:16:54 -0600

    Update deployrealtimechannel.sh

--- conf/channels/deployrealtimechannel.sh
+++ conf/channels/deployrealtimechannel.sh
@@ -2,10 +2,10 @@
 
 cd /opt/mirthconnect/
 
-result=$(java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/getchannellist | grep Realtime_HL7V2_Processor | wc -l)
+result=$(java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/mirth-cli-getchannellist | grep Realtime_HL7V2_Processor | wc -l)
 if [ $result -gt 0 ]; then
   echo "Realtime_HL7V2_Processor is already installed."
 else
-  java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/deployrealtimechannel
+  java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/mirth-cli-deployrealtimechannel
   echo "Installed Realtime_HL7V2_Processor."
 fi
