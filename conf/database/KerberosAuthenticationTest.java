b2feae2547b55dffbbda290f76025daa58baf621
mdpettit <marshall.pettit@healthcatalyst.com>
2017-09-22 14:05:36 -0600

    Add script to deploy database switch with integrated security

---
+++ conf/database/KerberosAuthenticationTest.java
@@ -0,0 +1,48 @@
+import java.sql.*;
+
+public class KerberosAuthenticationTest {
+  static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+  static final String DB_URL = "jdbc:sqlserver://$Server.$Domain:$Port;DatabaseName=master;integratedSecurity=true;authenticationScheme=JavaKerberos;serverSpn=MSSQLSvc/$Server.$Domain:$Port";
+  
+  public static void main(String[] args) {
+    Connection conn = null;
+    Statement stmt = null;
+    
+    try {
+      Class.forName(JDBC_DRIVER);
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL);
+      System.out.println("Creating statement...");
+      stmt = conn.createStatement();
+      String sql;
+      sql = "select auth_scheme from sys.dm_exec_connections where session_id=@@spid";
+      ResultSet rs = stmt.executeQuery(sql);
+      while(rs.next()) {
+        String value = rs.getString(1);
+        System.out.println("Authentication Scheme: " + value);
+      }
+      rs.close();
+      stmt.close();
+      conn.close();
+    } catch(SQLException se) {
+      se.printStackTrace();
+    } catch(Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if(stmt!=null) stmt.close();
+      } 
+      catch(SQLException se2) { 
+      } //Do nothing
+      try {
+        if(conn!=null) conn.close();
+      } 
+      catch(SQLException se3) { 
+        se3.printStackTrace(); 
+      }
+    }
+
+    System.out.println("Bye DBExample.");
+    
+  }
+}
---
+++ conf/database/switchtosqlserver.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# usage: ./switchtosqlserver.sh username password domain kdc database port catalog
+
+# capture parameters
+username=$1
+password=$2
+domain=$3
+ad_hostname=$4
+db_hostname=$5
+db_port=$6
+db_catalog=$7
+
+# echo parameters
+echo "Username: $username"
+echo "Domain: $domain"
+echo "Domain Controller: $ad_hostname"
+echo "Database Server: $db_hostname"
+echo "Database Port: $db_port"
+echo "Database Catalog: $db_catalog"
+
+# make lowercase and uppercase
+username_lc="${username,,}"
+domain_lc="${domain,,}"
+domain_uc="${domain^^}"
+
+# find and replace values in config files with parameters
+sed -i 's/$domain/'"$domain_lc"'/g' /opt/mirthconnect_database/krb5.conf
+sed -i 's/$DOMAIN/'"$domain_uc"'/g' /opt/mirthconnect_database/krb5.conf
+sed -i 's/$server/'"$ad_hostname"'/g' /opt/mirthconnect_database/krb5.conf
+
+sed -i 's/$username/'"$username_lc"'/g' /opt/mirthconnect_database/SQLJDBCDriver.conf
+sed -i 's/$DOMAIN/'"$domain_uc"'/g' /opt/mirthconnect_database/SQLJDBCDriver.conf
+
+sed -i 's/$Server/'"$db_hostname"'/g' /opt/mirthconnect_database/mirth.properties
+sed -i 's/$Port/'"$db_port"'/g' /opt/mirthconnect_database/mirth.properties
+sed -i 's/$Domain/'"$domain"'/g' /opt/mirthconnect_database/mirth.properties
+sed -i 's/$Database/'"$db_catalog"'/g' /opt/mirthconnect_database/mirth.properties
+
+sed -i 's/$Server/'"$db_hostname"'/g' /opt/mirthconnect_database/KerberosAuthenticationTest.java
+sed -i 's/$Port/'"$db_port"'/g' /opt/mirthconnect_database/KerberosAuthenticationTest.java
+sed -i 's/$Domain/'"$domain"'/g' /opt/mirthconnect_database/KerberosAuthenticationTest.java
+
+# swap Mirth Connect config files
+cp /opt/mirthconnect_database/krb5.conf /etc/krb5.conf
+cp /opt/mirthconnect_database/SQLJDBCDriver.conf /opt/mirthconnect/conf/SQLJDBCDriver.conf
+cp /opt/mirthconnect_database/mirth.properties /opt/mirthconnect/conf/mirth.properties
+cp /opt/mirthconnect_database/mcservice /opt/mirthconnect/mcservice
+cp /opt/mirthconnect_database/mcservice.vmoptions /opt/mirthconnect/mcservice.vmoptions
+
+# create keytab file
+ktutil <<EOF
+addent -password -p $username@$domain_uc -k 1 -e RC4-HMAC
+$password
+wkt /opt/mirthconnect/conf/mirth.keytab
+quit
+EOF
+
+# generate TGT
+kinit $username_lc@$domain_uc -k -t /opt/mirthconnect/conf/mirth.keytab
+klist
+
+# test integrated security
+cd /opt/mirthconnect_database/
+javac KerberosAuthenticationTest.java
+java -cp ".:/opt/mirthconnect/custom-lib/sqljdbc42.jar" -Djava.security.auth.login.config=/opt/mirthconnect/conf/SQLJDBCDriver.conf -Djava.security.krb5.conf=/etc/krb5.conf /opt/mirthconnect_database/KerberosAuthenticationTest
