b952c1ce304e0d918bbdab6af3e1359cdd0f4416
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-10-30 21:17:53 -0700

    pick up the username automatically from windows
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- runwithsql.cmd
+++ runwithsql.cmd
@@ -3,8 +3,8 @@ docker rm fabric.docker.interfaceengine
 docker pull healthcatalyst/fabric.baseos:latest
 docker build -t healthcatalyst/fabric.docker.interfaceengine . 
 
-set /P sqlusername=Enter Windows UserName: 
+@echo off
 
-set /P sqlpwd=Enter Windows Password: 
+set /P sqlpwd=Enter Windows Password for %USERNAME%: 
 
-docker run -p 8080:8080 -p 8443:8443 -p 6661:6661 -e SQLSERVER_USER=%sqlusername% -e SQLSERVER_PASSWORD=%sqlpwd%  -e SQLSERVER_DOMAIN=hqcatalyst.local -e SQLSERVER_AD_URL=hcsad1 -e SQLSERVER_SERVER=hc2034 -e SQLSERVER_DATABASE=MyRealtimeDb --rm --name fabric.docker.interfaceengine -t healthcatalyst/fabric.docker.interfaceengine
+docker run -p 8080:8080 -p 8443:8443 -p 6661:6661 -e SQLSERVER_USER=%USERNAME% -e SQLSERVER_PASSWORD=%sqlpwd%  -e SQLSERVER_DOMAIN=hqcatalyst.local -e SQLSERVER_AD_URL=hcsad1 -e SQLSERVER_SERVER=hc2034 -e SQLSERVER_DATABASE=MyRealtimeDb --rm --name fabric.docker.interfaceengine -t healthcatalyst/fabric.docker.interfaceengine
