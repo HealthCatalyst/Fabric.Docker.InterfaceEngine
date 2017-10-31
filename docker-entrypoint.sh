391f125c51d74e0d2bfc1e89b3523b11e069f3d7
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-10-30 21:09:57 -0700

    enable sending the Sql user name using secrets
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,20 @@ set -eu
 
 # set -x
 
-if [[ ! -z "${SQLSERVER_USER:-}" ]]; then
-    echo "SQLSERVER_USER is set so switching Mirth to use MySql"
+if [[ ! -z "${SQLSERVER_USER:-}" ]] || [[ ! -z "${SQLSERVER_USER_FILE:-}" ]]
+then
 
-    sqlserveruser=${SQLSERVER_USER}
+    echo "SQLSERVER_USER is set so switching Mirth to use MS SQL Server"
+
+    if [[ ! -z "${SQLSERVER_USER_FILE:-}" ]]
+    then
+        echo "SQLSERVER_USER_FILE is set so reading from $SQLSERVER_USER_FILE"
+        SQLSERVER_USER=$(cat $SQLSERVER_USER_FILE)
+    fi 
+    if [[ -z "${SQLSERVER_USER:-}" ]]; then
+        echo "SQLSERVER_USER must be specified"
+        exit 1
+    fi    
 
     if [[ ! -z "${SQLSERVER_PASSWORD_FILE:-}" ]]
     then
@@ -18,32 +28,50 @@ if [[ ! -z "${SQLSERVER_USER:-}" ]]; then
         echo "SQLSERVER_PASSWORD must be specified"
         exit 1
     fi
-    sqlserverpassword=${SQLSERVER_PASSWORD}
 
+    if [[ ! -z "${SQLSERVER_DOMAIN_FILE:-}" ]]
+    then
+        echo "SQLSERVER_DOMAIN_FILE is set so reading from $SQLSERVER_DOMAIN_FILE"
+        SQLSERVER_DOMAIN=$(cat $SQLSERVER_DOMAIN_FILE)
+    fi 
     if [[ -z "${SQLSERVER_DOMAIN:-}" ]]; then
         echo "SQLSERVER_DOMAIN must be specified"
         exit 1
-    fi
-    domain=${SQLSERVER_DOMAIN}
+    fi    
+
+    if [[ ! -z "${SQLSERVER_AD_URL_FILE:-}" ]]
+    then
+        echo "SQLSERVER_AD_URL_FILE is set so reading from $SQLSERVER_AD_URL_FILE"
+        SQLSERVER_AD_URL=$(cat $SQLSERVER_AD_URL_FILE)
+    fi 
     if [[ -z "${SQLSERVER_AD_URL:-}" ]]; then
         echo "SQLSERVER_AD_URL must be specified"
         exit 1
-    fi
-    adserver=${SQLSERVER_AD_URL}
-    if [[ -z "${SQLSERVER_SERVER:-}" ]]; then
-        echo "SQLSERVER_DOMAIN must be specified"
-        exit 1
-    fi
-    sqlserver=${SQLSERVER_SERVER}
-    sqlserverport=${SQLSERVER_PORT:-1433}
+    fi    
 
+    if [[ ! -z "${SQLSERVER_SERVER_FILE:-}" ]]
+    then
+        echo "SQLSERVER_SERVER_FILE is set so reading from $SQLSERVER_SERVER_FILE"
+        SQLSERVER_SERVER=$(cat $SQLSERVER_SERVER_FILE)
+    fi 
+    if [[ -z "${SQLSERVER_SERVER:-}" ]]; then
+        echo "SQLSERVER_SERVER must be specified"
+        exit 1
+    fi    
+
+    if [[ ! -z "${SQLSERVER_DATABASE_FILE:-}" ]]
+    then
+        echo "SQLSERVER_DATABASE_FILE is set so reading from $SQLSERVER_DATABASE_FILE"
+        SQLSERVER_DATABASE=$(cat $SQLSERVER_DATABASE_FILE)
+    fi 
     if [[ -z "${SQLSERVER_DATABASE:-}" ]]; then
         echo "SQLSERVER_DATABASE must be specified"
         exit 1
-    fi
-    sqlserverdatabase=${SQLSERVER_DATABASE}
+    fi    
 
-    /opt/mirthconnect_database/switchtosqlserver.sh $sqlserveruser $sqlserverpassword $domain $adserver $sqlserver $sqlserverport $sqlserverdatabase
+    # now run with all the parameters
+    /opt/mirthconnect_database/switchtosqlserver.sh ${SQLSERVER_USER} ${SQLSERVER_PASSWORD} ${SQLSERVER_DOMAIN} ${SQLSERVER_AD_URL} ${SQLSERVER_SERVER} ${SQLSERVER_PORT:-1433} ${SQLSERVER_DATABASE}
+
 else
     if [[ ! -z "${MYSQL_USER:-}" ]]; then
         echo "$MYSQL_USER is set so switching Mirth to use MySql"
@@ -52,9 +80,8 @@ else
     fi
 fi
 
-
 /opt/mirthconnect/startmirthandrenewcredentials.sh
 
-echo "starting final"
+echo "starting Mirth"
 
 exec "$@"
