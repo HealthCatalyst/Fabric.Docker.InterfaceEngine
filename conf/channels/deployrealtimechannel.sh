77f4b1775ed3551e484902a4a4882be96d68dc49
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-11-26 18:25:08 -0800

    deploy new channel even if the old channel exists otherwise we can't update a channel
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- conf/channels/deployrealtimechannel.sh
+++ conf/channels/deployrealtimechannel.sh
@@ -2,18 +2,8 @@
 
 cd /opt/mirthconnect/
 
-result=$(java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/mirth-cli-getchannellist | grep Realtime_HL7V2_Processor | wc -l)
-if [ $result -gt 0 ]; then
-  echo "Realtime_HL7V2_Processor is already installed."
-else
-  java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/mirth-cli-deployrealtimechannel
-  echo "Installed Realtime_HL7V2_Processor."
-fi
+java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/mirth-cli-deployrealtimechannel
+echo "Installed Realtime_HL7V2_Processor."
 
-result=$(java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/mirth-cli-getchannellist | grep TCP_Receiver_Channel | wc -l)
-if [ $result -gt 0 ]; then
-  echo "TCP_Receiver_Channel is already installed."
-else
-  java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/mirth-cli-deploytcpreceiverchannel
-  echo "Installed TCP_Receiver_Channel."
-fi
+java -cp "." -jar mirth-cli-launcher.jar -s /opt/mirthconnect_channels/mirth-cli-deploytcpreceiverchannel
+echo "Installed TCP_Receiver_Channel."
