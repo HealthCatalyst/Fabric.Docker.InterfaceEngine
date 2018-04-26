435fbdfa045c3dcc98d8f98b4700ca62a62657f5
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-04-26 11:26:34 -0700

    update runwithsql.cmd to get hostname from environment
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- runwithsql.cmd
+++ runwithsql.cmd
@@ -3,8 +3,12 @@ docker rm fabric.docker.interfaceengine
 docker pull healthcatalyst/fabric.baseos:latest
 docker build -t healthcatalyst/fabric.docker.interfaceengine . 
 
+FOR /F "usebackq" %%i IN (`hostname`) DO SET MyHOSTNAME=%%i
+ECHO %MyHOSTNAME%
+
 @echo off
 
 set /P sqlpwd=Enter Windows Password for %USERNAME%: 
 
-docker run -p 8080:8080 -p 8443:8443 -p 6661:6661 -e SQLSERVER_USER=%USERNAME% -e SQLSERVER_PASSWORD=%sqlpwd%  -e SQLSERVER_DOMAIN=hqcatalyst.local -e SQLSERVER_AD_URL=hcsad1 -e SQLSERVER_SERVER=hc2034 -e SQLSERVER_DATABASE=MyRealtimeDb --rm --name fabric.docker.interfaceengine -t healthcatalyst/fabric.docker.interfaceengine
+
+docker run -p 8080:8080 -p 8443:8443 -p 6661:6661 -e SQLSERVER_USER=%USERNAME% -e SQLSERVER_PASSWORD=%sqlpwd%  -e SQLSERVER_DOMAIN=hqcatalyst.local -e SQLSERVER_AD_URL=hcsad1 -e SQLSERVER_SERVER=%MYHOSTNAME% -e SQLSERVER_DATABASE=MyRealtimeDb --rm --name fabric.docker.interfaceengine -t healthcatalyst/fabric.docker.interfaceengine
