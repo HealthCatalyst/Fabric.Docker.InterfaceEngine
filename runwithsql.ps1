4d30c14de2b4b189299ad066fcb38ff9e93a5d84
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-08 21:56:21 -0700

    add comment about restart sql server after connecting to vpn
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- runwithsql.ps1
+++ runwithsql.ps1
@@ -8,6 +8,14 @@ while ([string]::IsNullOrWhiteSpace($password)) {
     $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
 }
 
-$SqlServerName="$env:COMPUTERNAME"
+$USERNAME=$($env:USERNAME)
+$AD_DOMAIN=$env:USERDNSDOMAIN
+$AD_DOMAIN_SERVER=$($env:LOGONSERVER).Replace("\\","")
+$SqlServerName="$env:computername"
 
-docker run -p 8080:8080 -p 8443:8443 -p 6661:6661 -e SQLSERVER_USER=$env:UserName -e SQLSERVER_PASSWORD=$password  -e SQLSERVER_DOMAIN=hqcatalyst.local -e SQLSERVER_AD_URL=hcsad1 -e SQLSERVER_SERVER=$SqlServerName -e SQLSERVER_DATABASE=FabricRealtime --rm --name fabric.docker.interfaceengine -t healthcatalyst/fabric.docker.interfaceengine
+Write-Host "======================================================================"
+Write-Host "IMPORTANT: If you're connecting to your local machine, make sure you are connected to VPN and restart your MS Sql service after connecting to VPN"
+Write-Host "(otherwise you will get Cannot create SSPI context errors)"
+Write-Host "======================================================================"
+
+docker run -p 8080:8080 -p 8443:8443 -p 6661:6661 -e SQLSERVER_USER=$env:UserName -e SQLSERVER_PASSWORD=$password  -e SQLSERVER_DOMAIN=$AD_DOMAIN -e SQLSERVER_AD_URL=$AD_DOMAIN_SERVER -e SQLSERVER_SERVER=$SqlServerName -e SQLSERVER_DATABASE=FabricRealtime --rm --name fabric.docker.interfaceengine -t healthcatalyst/fabric.docker.interfaceengine
