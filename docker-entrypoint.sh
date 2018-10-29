aba422fb330b3b42114600182ee2bba3e5cfef83
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-29 16:24:12 -0700

    remove contextpath
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-echo "Version 2018.04.10.01"
+echo "Version 2018.10.29.01"
 # set -x
 
 if [[ ! -z "${SQLSERVER_USER:-}" ]] || [[ ! -z "${SQLSERVER_USER_FILE:-}" ]]
@@ -14,17 +14,17 @@ then
     then
         echo "SQLSERVER_USER_FILE is set so reading from $SQLSERVER_USER_FILE"
         SQLSERVER_USER=$(cat $SQLSERVER_USER_FILE)
-    fi 
+    fi
     if [[ -z "${SQLSERVER_USER:-}" ]]; then
         echo "SQLSERVER_USER must be specified"
         exit 1
-    fi    
+    fi
 
     if [[ ! -z "${SQLSERVER_PASSWORD_FILE:-}" ]]
     then
         echo "SQLSERVER_PASSWORD_FILE is set so reading from $SQLSERVER_PASSWORD_FILE"
         SQLSERVER_PASSWORD=$(cat $SQLSERVER_PASSWORD_FILE)
-    fi 
+    fi
     if [[ -z "${SQLSERVER_PASSWORD:-}" ]]; then
         echo "SQLSERVER_PASSWORD must be specified"
         exit 1
@@ -34,41 +34,41 @@ then
     then
         echo "SQLSERVER_DOMAIN_FILE is set so reading from $SQLSERVER_DOMAIN_FILE"
         SQLSERVER_DOMAIN=$(cat $SQLSERVER_DOMAIN_FILE)
-    fi 
+    fi
     if [[ -z "${SQLSERVER_DOMAIN:-}" ]]; then
         echo "SQLSERVER_DOMAIN must be specified"
         exit 1
-    fi    
+    fi
 
     if [[ ! -z "${SQLSERVER_AD_URL_FILE:-}" ]]
     then
         echo "SQLSERVER_AD_URL_FILE is set so reading from $SQLSERVER_AD_URL_FILE"
         SQLSERVER_AD_URL=$(cat $SQLSERVER_AD_URL_FILE)
-    fi 
+    fi
     if [[ -z "${SQLSERVER_AD_URL:-}" ]]; then
         echo "SQLSERVER_AD_URL must be specified"
         exit 1
-    fi    
+    fi
 
     if [[ ! -z "${SQLSERVER_SERVER_FILE:-}" ]]
     then
         echo "SQLSERVER_SERVER_FILE is set so reading from $SQLSERVER_SERVER_FILE"
         SQLSERVER_SERVER=$(cat $SQLSERVER_SERVER_FILE)
-    fi 
+    fi
     if [[ -z "${SQLSERVER_SERVER:-}" ]]; then
         echo "SQLSERVER_SERVER must be specified"
         exit 1
-    fi    
+    fi
 
     if [[ ! -z "${SQLSERVER_DATABASE_FILE:-}" ]]
     then
         echo "SQLSERVER_DATABASE_FILE is set so reading from $SQLSERVER_DATABASE_FILE"
         SQLSERVER_DATABASE=$(cat $SQLSERVER_DATABASE_FILE)
-    fi 
+    fi
     if [[ -z "${SQLSERVER_DATABASE:-}" ]]; then
         echo "SQLSERVER_DATABASE must be specified"
         exit 1
-    fi    
+    fi
 
     # now run with all the parameters
     /opt/mirthconnect_database/switchtosqlserver.sh ${SQLSERVER_USER} ${SQLSERVER_PASSWORD} ${SQLSERVER_DOMAIN} ${SQLSERVER_AD_URL} ${SQLSERVER_SERVER} ${SQLSERVER_PORT:-1433} ${SQLSERVER_DATABASE}
